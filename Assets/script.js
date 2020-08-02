1acd6306a26bc08b8ec5b436c7b069abfce6c11a
Grant Pardington <grant.pardington@gmail.com>
2020-08-02 17:15:37 -0600

    More JS

--- Assets/script.js
+++ Assets/script.js
@@ -54,3 +54,68 @@ function searchCity(city){
 $.ajax({
     url: queryURL,
     method: "GET"
+}).then(function(response){
+    // Setting variables for weather results 
+    // Display the current search on main div
+    $("#cityName").attr("class", "nowrap").text(city);
+    let tempT = $("#temperature").attr("class", "nowrap");
+    $("#humidity").attr("class", "nowrap").text("Humidity: "+ response.main.humidity + "%");
+    $("#windSpeed").attr("class", "nowrap").text("Wind Speed: " + response.wind.speed + " MPH");
+    
+    // Get the date and time and set it
+    let today = new Date();
+    let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date+' '+time;
+
+    // Displaying the date when the city is selected.
+    $("#currentDate").text(dateTime);
+
+    // Show Temperature on main div
+    tempT.text("Temperature: "+ cTemp);
+
+    // Variables for ajax call for UV response
+    let cityLat = response.coord.lat;
+    let cityLon = response.coord.lon;
+    let uvURL = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/uvi?appid=" + "29d9121dacef0624c94a4b33f6e86502" + "&lat=" + cityLat + "&lon=" + cityLon + "&units=imperial";
+    $.ajax({
+        url: uvURL,
+        method: "GET"
+    }).then(function (response) {
+
+         // Create variable to get the UV and to create dom element on div.
+         let uv = response.value;
+         $("#uvIndex").empty(); // clear the element first
+         $("#uvIndex").append($("<div id=\"uvColor\">").text("Uv Index: " + uv)).attr("class", "nowrap");
+         // When checking the UV create a color that shows whether the conditions are good, moderate, or bad
+         if(uv <= 3){
+             //change color to green
+             $("#uvColor").attr("style", "background-color:green ; width:65%");
+         }
+         else if( uv <= 7){
+             //change color to yellow
+             $("#uvColor").attr("style", "background-color:yellow ; width:65%");
+         }
+         else{
+             //change color to red
+             $("#uvColor").attr("style", "background-color:red ; width:65%");
+         };
+         
+         })
+    })
+ };
+// Display the future weather conditions for the searched city and show a 5-day forecast that displays the date, an icon representation of weather conditions, wind, UV, temperature, and humidity
+function forecast(city) {
+    // Section for the 5 day query
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=3d16044a2eba4d271046d70fd1f2c155";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        // forecastFunction
+    }).then(function(forecastResponse){
+        // Creating  a filter for the 5 days
+        let filteredDays = forecastResponse.list.filter(
+            function (currentElement){
+            return currentElement.dt_txt.includes("12:00:00")
+            }	
+        );
